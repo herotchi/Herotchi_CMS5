31784c626dd7a0bbace1e09a81e7e68cace6347d
herotchi <hidenochion+github@gmail.com>
2024-12-19 15:16:13 +0900

    bootstrapの導入

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,8 +11,12 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Styles -->
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
         <!-- Scripts -->
         {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
+        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
     </head>
     <body class="">
         <div class="">
