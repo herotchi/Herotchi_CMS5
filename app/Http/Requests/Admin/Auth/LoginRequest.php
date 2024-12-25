057e50646bbe5731ce2cc191eacebb344c7e5f95
herotchi <hidenochion+github@gmail.com>
2024-12-25 16:17:45 +0900

    サイト管理者用認証処理を実装

---
+++ app/Http/Requests/Admin/Auth/LoginRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests\Admin\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+            'login_id' => ['required', 'string'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function authenticate(): void
+    {
+        if (! Auth::guard('admin')->attempt($this->only('login_id', 'password'))) {
+
+            throw ValidationException::withMessages([
+                'login_id' => trans('auth.failed'),
+            ]);
+        }
+
+    }
+}
