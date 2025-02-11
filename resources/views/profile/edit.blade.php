6765e95ab4989d992f913e38baefa0d597bda371
herotchi <hidenochion+github@gmail.com>
2025-02-11 12:54:16 +0900

    公開側アカウント削除画面の実装

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -19,19 +19,23 @@
         @include('profile.partials.update-password-form')
     </div>
 
-    <div class="">
+    <div class="card shadow-sm my-4">
+        @include('profile.partials.delete-user-form')
+    </div>
+
+    {{--<div class="">
         <div class="">
-            {{--<div class="">
+            <div class="">
                 <div class="">
                     @include('profile.partials.update-profile-information-form')
                 </div>
-            </div>--}}
+            </div>
 
-            {{--<div class="">
+            <div class="">
                 <div class="">
                     @include('profile.partials.update-password-form')
                 </div>
-            </div>--}}
+            </div>
 
             <div class="">
                 <div class="">
@@ -39,5 +43,5 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div>--}}
 </x-app-layout>
--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -1,5 +1,5 @@
 <section class="space-y-6">
-    <header>
+    {{--<header>
         <h2 class="">
             {{ __('Delete Account') }}
         </h2>
@@ -7,14 +7,104 @@
         <p class="">
             {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
         </p>
-    </header>
+    </header>--}}
 
-    <x-danger-button
+    <div class="card-header">{{ __('Delete Account') }}</div>
+    <div class="card-body">
+        <p class="mb-0">
+            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+        </p>
+    </div>
+    <div class="card-footer">
+        <div class="row">
+            <div class="col-12 my-2">
+                <!-- Button trigger modal -->
+                <x-danger-button  data-bs-toggle="modal" data-bs-target="#destroyModal">
+                    {{ __('Delete Account') }}
+                </x-danger-button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal @if($errors->userDeletion->has('password'))@else fade @endif" id="destroyModal" tabindex="-1" aria-labelledby="destroyModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <form method="POST" action="{{ route('profile.destroy') }}" novalidate>
+                    @method('DELETE')
+                    @csrf
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="destroyModalLabel">{{ __('Are you sure you want to delete your account?') }}</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p>
+                            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+                        </p>
+                        <div class="row g-3">
+                            <!-- パスワード -->
+                            <div class="col-md-12">
+                                <x-text-input
+                                    id="deleteUserPassword" name="password" type="password"
+                                    class="form-control{{ $errors->userDeletion->has('password') ? ' is-invalid' : '' }}"
+                                    placeholder="{{ __('Password') }}"
+                                />
+                                <x-input-error :message="$errors->userDeletion->first('password')" class="" />
+                            </div>
+                        </div>
+                        {{--<x-input-label for="password" value="{{ __('Password') }}" class="" />
+
+                        <x-text-input
+                            id="password"
+                            name="password"
+                            type="password"
+                            class=""
+                            placeholder="{{ __('Password') }}"
+                        />
+
+                        <x-input-error :messages="$errors->userDeletion->get('password')" class="" />
+                        <x-input-error :message="$errors->userDeletion->first('password')" class="" />--}}
+                    </div>
+                    <div class="modal-footer justify-content-center">
+                        <button type="submit" class="btn btn-danger w-50">削除する</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">戻る</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script>
+        const myModalEl = document.getElementById('destroyModal');
+        const myBsModalEl = new bootstrap.Modal(myModalEl);
+        const myInputEl = document.getElementById('deleteUserPassword');
+        
+        // モーダルを閉じ終わったら入力エラークラスを削除する
+        myModalEl.addEventListener('hidden.bs.modal', event => {
+            myInputEl.classList.remove('is-invalid');
+        });
+
+        // モーダルを閉じる瞬間にフェード演出を追加する
+        myModalEl.addEventListener('hide.bs.modal', event => {
+            myModalEl.classList.add('fade');
+        });
+        
+        // ページ読み込み時にモーダルを表示
+        document.addEventListener('DOMContentLoaded', function () {
+            // 入力エラーがあった場合
+            @if ($errors->userDeletion->has('password'))
+                myBsModalEl.show();
+            @endif
+        });
+        
+    </script>
+
+
+    {{--<x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
-    >{{ __('Delete Account') }}</x-danger-button>
+    >{{ __('Delete Account') }}</x-danger-button>--}}
 
-    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+    {{--<x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
@@ -38,7 +128,7 @@
                     placeholder="{{ __('Password') }}"
                 />
 
-                {{--<x-input-error :messages="$errors->userDeletion->get('password')" class="" />--}}
+                <x-input-error :messages="$errors->userDeletion->get('password')" class="" />
                 <x-input-error :message="$errors->userDeletion->first('password')" class="" />
             </div>
 
@@ -52,5 +142,5 @@
                 </x-danger-button>
             </div>
         </form>
-    </x-modal>
+    </x-modal>--}}
 </section>
