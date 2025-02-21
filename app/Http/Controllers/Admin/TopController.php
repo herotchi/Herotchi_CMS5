d1d9c63e56128a1f07f770207ec5a96ee884ed6a
herotchi <hidenochion+github@gmail.com>
2025-02-21 16:20:56 +0900

    管理側トップ画面の実装

--- app/Http/Controllers/Admin/TopController.php
+++ app/Http/Controllers/Admin/TopController.php
@@ -6,11 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
+use App\Models\News;
+use App\Models\Media;
+
 class TopController extends Controller
 {
     //
     public function index(): View
     {
-        return view('admin.top');
+        $newsModel = new News();
+        $news = $newsModel->getTopNews();
+
+        $mediaModel = new Media();
+        $carousels = $mediaModel->getCarousels();
+        $pickUps = $mediaModel->getPickUps();
+
+        return view('admin.top', compact('news', 'carousels', 'pickUps'));
     }
 }
